20ca5b73d6d6967d29a69afddc88060710c37283
= <=>
2025-08-19 15:57:28 +0330

    creat a method in usercontroller for signUp

--- restaurant-order/src/main/java/Controller/UserController.java
+++ restaurant-order/src/main/java/Controller/UserController.java
@@ -1,7 +1,21 @@
 package Controller;
 
-public class UserController {
-    public void signUp(String userName, String password) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class UserController {
+    public void signUp(String userName, String password) throws SQLException {
+        String URL = "jdbc:postgresql://localhost:5432/Restaurant_Order";
+        String USER = "RestaurantAdmin";
+        String PASS = "RestaurantAdmin";
+        Connection con = DriverManager.getConnection(URL, USER, PASS);
+        String sql = "INSERT INTO users (userName, password) VALUES (?,?)";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, userName);
+        ps.setString(2, password);
+        ps.executeUpdate();
+        con.close();
     }
 }
