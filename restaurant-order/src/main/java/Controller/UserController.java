953cfe4753e31d1fd168740453a6d3724006af19
= <=>
2025-08-19 16:39:13 +0330

    creat a method for login

--- restaurant-order/src/main/java/Controller/UserController.java
+++ restaurant-order/src/main/java/Controller/UserController.java
@@ -1,9 +1,6 @@
 package Controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UserController {
     public void signUp(String userName, String password) throws SQLException {
@@ -11,11 +8,29 @@ public class UserController {
         String USER = "RestaurantAdmin";
         String PASS = "RestaurantAdmin";
         Connection con = DriverManager.getConnection(URL, USER, PASS);
-        String sql = "INSERT INTO users (userName, password) VALUES (?,?)";
+        String sql = "INSERT INTO users (username, password) VALUES (?,?)";
         PreparedStatement ps = con.prepareStatement(sql);
         ps.setString(1, userName);
         ps.setString(2, password);
         ps.executeUpdate();
         con.close();
+    }
+    public boolean login(String userName, String password) throws SQLException {
+        String URL = "jdbc:postgresql://localhost:5432/Restaurant_Order";
+        String USER = "RestaurantAdmin";
+        String PASS = "RestaurantAdmin";
+        Connection con = DriverManager.getConnection(URL, USER, PASS);
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, userName);
+        ps.setString(2, password);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            con.close();
+            return true;
+        }
+        con.close();
+        return false;
+
     }
 }
